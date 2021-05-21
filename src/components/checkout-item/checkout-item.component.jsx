0e624db48179bb65d5e8fc9eef288582e3e16a16
Florin Lilea <florin.lilea94@gmail.com>
2021-05-21 11:50:17 +0200

    udated the reducer to add the quantity arrow addition

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,17 +1,36 @@
 import React from 'react';
 import './checkout-item.styles.scss';
-const CheckoutItem = ({cartItem:{name,price,imageUrl,quantity}}) => {
+import { connect } from 'react-redux';
+import { removeItem, addItem, clearItem } from '../../redux/cart/cart.actions';
+const CheckoutItem = ({ cartItem, removeCartItem, removeItem, addItem }) => {
+  const { name, price, imageUrl, quantity } = cartItem;
   return (
     <div className="checkout-item">
       <div className="image-container">
-          <img src={imageUrl} alt='item'/>
+        <img src={imageUrl} alt="item" />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <span className="quantity">
+        <div onClick={() => removeItem(cartItem)} className="arrow">
+          {' '}
+          &#10094;
+        </div>
+        <span className="value">{quantity}</span>
+        <div onClick={() => addItem(cartItem)} className="arrow">
+          &#10095;
+        </div>
+      </span>
       <span className="price">{price}</span>
-     <div className="remove-button">&#10005;</div>
+      <div className="remove-button" onClick={() => removeCartItem(cartItem)}>
+        &#10005;
+      </div>
     </div>
   );
 };
+const mapDispatchToProps = (dispatch) => ({
+  removeCartItem: (item) => dispatch(removeItem(item)),
+  addItem: (item) => dispatch(addItem(item)),
+  removeItem: (item) => dispatch(clearItem(item)),
+});
 
-export default CheckoutItem;
+export default connect(null, mapDispatchToProps)(CheckoutItem);
